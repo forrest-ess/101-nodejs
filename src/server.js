54baaf4c7f245e5ba1532b451278d5832816296e
Forrest <forrest.ess@gmail.com>
2024-10-14 22:48:03 +0900

    ejs

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
-const express = require("express");
+//const express = require("express");
+import express from "express"
+import configViewEngine from "./configs/viewEngine";
+
+
+const path = require('path')
 const app = express();
 const port = 3000;
 
+
+configViewEngine(app);
+
 app.get("/", (req, res) => {
-  res.send("Hello Forrest!");
+  res.render('index.ejs')
 });
 
 app.get("/about", (req, res) => {
