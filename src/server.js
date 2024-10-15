1563de58e6b2241b43a77fd2cc4c5495c61b588b
Forrest <forrest.ess@gmail.com>
2024-10-15 10:55:09 +0900

    static files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 //const express = require("express");
 import express from "express"
 import configViewEngine from "./configs/viewEngine";
-
+require('dotenv').config();
 
 const path = require('path')
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 
 configViewEngine(app);
